766fec91b08e4a414e217a52ec348281cbb1d9e7
ManApart <r@austinkucera.com>
2022-08-16 08:01:53 -0400

    vortex extension initial commit

---
+++ VortexExtension/index.js
@@ -0,0 +1,13 @@
+//Import some assets from Vortex we'll need.
+const path = require('path');
+const { fs, log, util } = require('vortex-api');
+
+function main(context) {
+	//This is the main function Vortex will run when detecting the game extension. 
+	
+	return true
+}
+
+module.exports = {
+    default: main,
+  };
