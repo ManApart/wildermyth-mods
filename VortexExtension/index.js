1123657ba3d6ab5ab239819aa360d9a4972fd29d
ManApart <r@austinkucera.com>
2022-08-16 08:04:48 -0400

    game ids

--- VortexExtension/index.js
+++ VortexExtension/index.js
@@ -2,6 +2,11 @@
 const path = require('path');
 const { fs, log, util } = require('vortex-api');
 
+// Nexus Mods domain for the game. e.g. nexusmods.com/bloodstainedritualofthenight
+const GAME_ID = 'wildermyth';
+const STEAMAPP_ID = '763890';
+const GOGAPP_ID = '1853330157';
+
 function main(context) {
 	//This is the main function Vortex will run when detecting the game extension. 
 	
