c7cb6c2226a0e4eeedaa7be26f3b5a5628cad4ad
ManApart <r@austinkucera.com>
2022-08-18 07:57:38 -0400

    backup script

---
+++ backup.kts
@@ -0,0 +1,29 @@
+//kotlinc -script backup.kts
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+
+val modNames = listOf("LOTRNames", "StraightUp", "EngagingEvents")
+
+val paths = File("config.txt").readLines()
+
+val modDirectory = paths
+    .map { File(it.split("\\").joinToString(File.separator)) }
+    .first { it.exists() }
+
+println("Backing up Files from ${modDirectory.absolutePath}")
+
+modDirectory.listFiles()
+    .filter { it.name in modNames }
+    .forEach { mod ->
+        val dest = Path.of(".", mod.name)
+        dest.toFile().mkdirs()
+        copyDir(mod.toPath(), dest)
+    }
+
+fun copyDir(src: Path, dest: Path) {
+    Files.walk(src).filter { !it.toFile().isDirectory }.forEach {
+        Files.copy(it, dest.resolve(src.relativize(it)), StandardCopyOption.REPLACE_EXISTING)
+    }
+}
